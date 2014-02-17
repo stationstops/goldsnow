c7f0cd321f7549ae35c7c66da67561b67572f3c3
Chris <chris@schoenfeld.com>
2014-02-16 19:16:31 -0500

    Created express app in root dir

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
 var app = express();
 
-app.use(logfmt.requestLogger());
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-redis.set('foo', 'bar');
-
-redis.get('foo', function(err, value) {
-  console.log('foo is: ' + value);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
