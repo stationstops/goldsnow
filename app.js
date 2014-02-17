8ab32c30e83b98f031729ec11242b416842b75e5
Chris <chris@schoenfeld.com>
2014-02-16 19:05:57 -0500

    web.js is now app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -12,4 +13,10 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+redis.set('foo', 'bar');
+
+redis.get('foo', function(err, value) {
+  console.log('foo is: ' + value);
 });
