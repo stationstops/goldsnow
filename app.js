c95d17c03bf37d6c18d9a9add1d27e45b95cc13e
Chris <chris@schoenfeld.com>
2014-02-16 20:38:10 -0500

    swig works

--- app.js
+++ app.js
@@ -4,17 +4,22 @@
  */
 
 var express = require('express');
+var swig = require('swig');    
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
 
 // all environments
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -23,14 +28,39 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', __dirname + '/views');
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+redis.set('foo', 'bar');
+
+redis.get('foo', function(err, value) {
+  console.log('foo is: ' + value);
+});
+
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+// app.get('/users', user.list);
+
+// app.get('/', function (req, res) {
+// res.render('index', { /* template locals context */ });
+// });
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
